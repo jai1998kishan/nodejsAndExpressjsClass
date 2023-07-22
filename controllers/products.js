4582c5f43ad2af6aedaf7dbf0b0ff691f999e2f0
jai1998kishan <jaikishangupta754@gmail.com>
2023-07-22 19:25:30 +0530

    mvc_Method

---
+++ controllers/products.js
@@ -0,0 +1,40 @@
+
+const Product = require('../models/product');
+
+
+// const products = [];
+
+exports.getAddProduct = (req, res, next) => {
+    res.render('add-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      formsCSS: true,
+      productCSS: true,
+      activeAddProduct: true
+    });
+};
+
+
+exports.postProduct = (req, res, next) => {
+    // products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
+    res.redirect('/');
+};
+
+
+exports.getProduct = (req, res, next) => {
+    Product.fetchAll((products)=>{
+      res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+      });
+    });
+    
+  }
+
+
